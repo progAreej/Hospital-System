ebc81a7431e599c57c0afdd9873a179044a777cb
Areej Omar Abumuhfouz <areejabumahfouz@gmail.com>
2024-06-12 10:07:02 +0300

    added by areej

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ function DisplayPatients(fullName, password, birthday, gender, diseases, phone) 
 function render(event) {
     event.preventDefault();
 
-  
     let fullName = document.getElementById("fullName").value;
     let password = document.getElementById("password").value;
     let birthday = document.getElementById("birthday").value;
@@ -20,43 +19,42 @@ function render(event) {
     let diseases = document.getElementById("diseases").value;
     let phone = document.getElementById("phone").value;
 
-    var arr = [
-        "Full name" + fullName,
-        "Password " + password,
-        "Birthday " + birthday,
-        "Gender " + gender,
-        "Diseases " + diseases,
-        "Phone " + phone
-    ];
-
     let num = localStorage.length + 1;
-    let patientKey = "Patient (" +num+')';
-    localStorage.setItem(patientKey, JSON.stringify(arr));
+    let patientKey = "Patient ("+ num+")";
+    let patientData = new DisplayPatients(fullName, password, birthday, gender, diseases, phone);
+    localStorage.setItem(patientKey, JSON.stringify(patientData));
+    let retrievedPatientData = JSON.parse(localStorage.getItem(patientKey));
+    const patient = new DisplayPatients(
+        retrievedPatientData.fullName,
+        retrievedPatientData.password,
+        retrievedPatientData.birthday,
+        retrievedPatientData.gender,
+        retrievedPatientData.diseases,
+        retrievedPatientData.phone
+    );
 
-    const patient = new DisplayPatients(fullName, password, birthday, gender, diseases, phone);
-    localStorage.setItem(patientKey, JSON.stringify(patient));
+    console.log(patient);
+
     let output = document.getElementById("output");
-
-    let card=document.createElement("div");
+    let card = document.createElement("div");
     output.appendChild(card);
-    card.style.backgroundColor="#7774a8";
-    card.style.color="white";
-    card.style.width="300px";
-    card.style.padding="20px";
-    card.style.margin="20px";
-    card.style.borderRadius="10px";
-    card.style.fontSize="25px";
-    card.style.fontFamily=" Arial, 'Helvetica Neue', Helvetica, sans-serif";
-    
-    card.innerHTML = 
-    '<img src="./Patient.jpg" width="300px">'+"<br>"+
-    "Full name is: " + patient.fullName+"<br>"+
-    "Password is: " + patient.password+"<br>"+
-    "Birthday is: " + patient.birthday+"<br>"+
-    "Gender is: " + patient.gender+"<br>"+
-    "Diseases is: " + patient.diseases+"<br>"+
-    "Phone  is: " + patient.phone+"<br>";
+    card.style.backgroundColor = "#7774a8";
+    card.style.color = "white";
+    card.style.width = "300px";
+    card.style.padding = "20px";
+    card.style.margin = "20px";
+    card.style.borderRadius = "10px";
+    card.style.fontSize = "20px";
+    card.style.fontFamily = "Arial, 'Helvetica Neue', Helvetica, sans-serif";
+
+    card.innerHTML =
+        '<img src="./Patient.jpg" width="300px" style="border-radius: 10px;">' + "<br>" + "<br>" +
+        "Full name: " + patient.fullName + "<br>" +
+        "Password: " + patient.password + "<br>" +
+        "Birthday: " + patient.birthday + "<br>" +
+        "Gender: " + patient.gender + "<br>" +
+        "Diseases: " + patient.diseases + "<br>" +
+        "Phone: " + patient.phone + "<br>";
 
     event.target.reset();
 }
- 
